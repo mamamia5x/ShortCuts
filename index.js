9829f099a36f4ce8faa242685c9653d3ee8ef2cf
Bugs Bunny <57536929+mamamia5x@users.noreply.github.com>
2020-10-05 10:42:39 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("test");
+module.exports.rannum = function (min, max){
+  return Math.floor(Math.random() * (max - min + 1) ) + min;
+}
+
+module.exports.remove = function(a,what) {
+    var what, a = arguments, L = a.length, ax;
+    while (L && this.length) {
+        what = a[--L];
+        while ((ax = this.indexOf(what)) !== -1) {
+            this.splice(ax, 1);
+        }
+    }
+    return this;
+};
